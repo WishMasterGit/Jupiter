3a1cb2c6fe5d53481596cbc1ae1eb8017771bc73
WishmasterTS <wishmasterts@gmail.com>
2026-03-01 17:51:17 -0500

    add icon

--- crates/jupiter-gui/src/main.rs
+++ crates/jupiter-gui/src/main.rs
@@ -5,6 +5,7 @@ mod panels;
 mod progress;
 mod states;
 mod workers;
+use image;
 
 fn main() -> eframe::Result<()> {
     tracing_subscriber::fmt()
@@ -14,13 +15,34 @@ fn main() -> eframe::Result<()> {
         )
         .init();
 
+    let icon_data = load_icon();
+
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([1280.0, 800.0])
             .with_min_inner_size([800.0, 600.0])
-            .with_title("Jupiter"),
+            .with_title("Jupiter")
+            .with_icon(std::sync::Arc::new(icon_data)),
         ..Default::default()
     };
+
+    fn load_icon() -> egui::IconData {
+        let (icon_rgba, icon_width, icon_height) = {
+            let icon_bytes = include_bytes!("../Jupiter256.png");
+            let image = image::load_from_memory(icon_bytes)
+                        .expect("Failed to load icon")
+                        .into_rgba8();
+            let (width, height) = image.dimensions();
+            (image.into_raw(), width, height)
+        };
+
+        egui::IconData {
+            rgba: icon_rgba,
+            width: icon_width,
+            height: icon_height
+        }
+
+    }
 
     eframe::run_native(
         "JupiterStack",
